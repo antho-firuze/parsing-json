36d6dcb1cbd142d2e552a30952852f3d3e89afac
antho-firuze <antho.firuze@gmail.com>
2018-05-05 04:51:21 +0700

    Initial commit

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1Button1 = new C1.Win.C1Input.C1Button();
+            this.c1Editor1 = new C1.Win.C1Editor.C1Editor();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Editor1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1Button1
+            // 
+            this.c1Button1.Location = new System.Drawing.Point(12, 12);
+            this.c1Button1.Name = "c1Button1";
+            this.c1Button1.Size = new System.Drawing.Size(75, 23);
+            this.c1Button1.TabIndex = 0;
+            this.c1Button1.Text = "c1Button1";
+            this.c1Button1.UseVisualStyleBackColor = true;
+            this.c1Button1.Click += new System.EventHandler(this.c1Button1_Click);
+            // 
+            // c1Editor1
+            // 
+            this.c1Editor1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.c1Editor1.Location = new System.Drawing.Point(12, 86);
+            this.c1Editor1.Name = "c1Editor1";
+            this.c1Editor1.Size = new System.Drawing.Size(412, 200);
+            this.c1Editor1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 298);
+            this.Controls.Add(this.c1Editor1);
+            this.Controls.Add(this.c1Button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.c1Button1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c1Editor1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private C1.Win.C1Input.C1Button c1Button1;
+        private C1.Win.C1Editor.C1Editor c1Editor1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void c1Button1_Click(object sender, EventArgs e)
+        {
+            GetRequest("http://www.infovesta.com/index2/mutualfund/SH%20/%200");
+        }
+
+        async void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        c1Editor1.Text = mycontent;
+                        deserialiseJSON(mycontent);
+                    }
+                }
+            }
+        }
+
+        private void deserialiseJSON(string strJSON) 
+        {
+            try
+            {
+                var o = JsonConvert.DeserializeObject<dynamic>(strJSON);
+                
+                debug("Here's our JSON object: " + o.ToString());
+                return;
+            }
+            catch(Exception e)
+            {
+                debug("We had a problem: " + e.Message.ToString());
+            }
+        }
+
+        private void debug(string s)
+        {
+            System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        private void output(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
